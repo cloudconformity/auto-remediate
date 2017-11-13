99e4da9e5c37d39165153c57abf5d03b7117ba3c
Mike <mike@cloudconformity.com>
2017-11-13 11:31:59 +1100

    Added a new auto-remediate: AutoRemediateConfig-001

--- functions/config.js
+++ functions/config.js
@@ -1,4 +1,12 @@
 module.exports = {
+
+	//Whether or not the auto-remediate function is enabled
 	"AutoRemediateRDS-008": true,
-	"AutoRemediateS3-001": true
+	"AutoRemediateS3-001": true,
+	"AutoRemediateConfig-001": true,
+
+
+	// Configure S3 bucket for AWS Config. This bucket receives configuration history and configuration snapshot files, which contain details for the resources that AWS Config records.
+	"CONFIG_S3_BUCKET": "",
+
 }
