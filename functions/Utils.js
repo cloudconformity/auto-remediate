68a15780a37de267f865c21ff427170cc05cbf12
bretwaldow <bret.waldow@mechanicalrock.io>
2017-11-14 18:50:00 +0800

    Resolve upstream conflicts

---
+++ functions/Utils.js
@@ -0,0 +1,21 @@
+const AWS = require("aws-sdk");
+
+module.exports = {
+
+	/**
+	 * <b>Returns AWS Account ID whose credentials are used to call the API</b>
+	 *
+	 */
+	getAccountId: function() {
+
+		let STS = new AWS.STS({apiVersion: '2011-06-15'});
+
+		return STS.getCallerIdentity({}).promise().then(function(data) {
+
+			return data.Account;
+
+		});
+
+	}
+
+};
--- functions/config.js
+++ functions/config.js
@@ -1,8 +1,16 @@
 module.exports = {
+
+	//Whether or not the auto-remediate function is enabled
 	"AutoRemediateRDS-008": true,
 	"AutoRemediateS3-001": true,
 	"AutoRemediateCT-001": true,
-	
+	"AutoRemediateConfig-001": true,
+
+
+	// Configure S3 bucket for AWS Config. This bucket receives configuration history and configuration snapshot files, which contain details for the resources that AWS Config records.
+	"CONFIG_S3_BUCKET": "",
+
+	// Configuration options for auto-remediating CloudTrail not global issue
 	"CT-001": {
 		"Name": 'GlobalTrail',
 		"S3BucketName": 'cc-remediate-cloudtrail',
@@ -10,4 +18,5 @@ module.exports = {
 		"IsMultiRegionTrail": true,
 		"S3KeyPrefix": 'cloudtrail-global'
 	}
+
 }
