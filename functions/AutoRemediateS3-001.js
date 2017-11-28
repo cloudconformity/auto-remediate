4a267a532cb79720479f0a4c8a863fb90567836b
bretwaldow <bret.waldow@mechanicalrock.io>
2017-11-28 09:15:15 +0800

    Include callback to fail function if process does not succeed

--- functions/AutoRemediateS3-001.js
+++ functions/AutoRemediateS3-001.js
@@ -29,7 +29,10 @@ module.exports.handler = (event, context, callback) => {
       ACL: "private",
     };
     s3.putBucketAcl(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
+      if (err) {
+				console.log(err, err.stack);
+				callback(err, "Failed to auto-remediate S3-001");
+			};
       else     console.log(data);           // successful response
     });
 
