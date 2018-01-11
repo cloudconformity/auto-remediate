e3b009b626752241280d143abafee2776dfffd0b
bretwaldow <bret.waldow@mechanicalrock.io>
2018-01-11 16:02:05 +0800

    First use of promises

--- functions/AutoRemediateS3-001.js
+++ functions/AutoRemediateS3-001.js
@@ -6,36 +6,47 @@ const AWS = require("aws-sdk");
 //S3BucketPublicReadAccess
 module.exports.handler = (event, context, callback) => {
 
-	console.log('S3 BucketPublicReadAccess - Received event:', JSON.stringify(event, null, 2));
+  console.log('S3 BucketPublicReadAccess - Received event:', JSON.stringify(event, null, 2));
 
-		if (!event || !event.resource || event.ruleId !== "S3-001") {
-			return handleError("Invalid event");
-		}
+  if (!event || !event.resource || event.ruleId !== "S3-001") {
+    return handleError("Invalid event");
+  }
 
-    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-    // this code is included to provide a record for debugging
-    var params = {
-      Bucket: event.resource
-    };
-    s3.getBucketAcl(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
+  // this code is included to provide a record for debugging
+  var params = {
+    Bucket: event.resource
+  };
+  let getAclPromise = s3.getBucketAcl(params).promise();
 
-    // actually close off access
-    var params = {
-      Bucket: event.resource,
-      ACL: "private",
-    };
-    s3.putBucketAcl(params, function(err, data) {
-      if (err) {
-				console.log(err, err.stack);
-				callback(err, "Failed to auto-remediate S3-001");
-			};
-      else     console.log(data);           // successful response
-    });
+  getAclPromise
+    .then(() => { console.log(data) })
+    .catch(() => { console.log(err, err.stack) });
+  // s3.getBucketAcl(params, function(err, data) {
+  //   if (err) console.log(err, err.stack); // an error occurred
+  //   else     console.log(data);           // successful response
+  // });
 
-	callback(null, "Success");
+  // actually close off access
+  var params = {
+    Bucket: event.resource,
+    ACL: "private",
+  };
+  let putAclPromise = s3.putBucketAcl(params).promise();
+
+  putAclPromise
+    .then(() => { console.log(data) })
+    .catch(() => { console.log(err, err.stack) });
+  // s3.putBucketAcl(params, function(err, data) {
+  //   if (err) {
+  //            console.log(err, err.stack);
+  //            callback(err, "Failed to auto-remediate S3-001");
+  //    };
+  //   else     console.log(data);           // successful response
+  // });
+
+  callback(null, "Success");
 
 };
+
