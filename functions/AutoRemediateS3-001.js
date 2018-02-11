b722877413e5b7dd330315016228773c1101c250
bretwaldow <bret.waldow@mechanicalrock.io>
2018-02-11 21:45:57 +0800

    function now ONLY changes READ status

--- functions/AutoRemediateS3-001.js
+++ functions/AutoRemediateS3-001.js
@@ -1,9 +1,31 @@
 "use strict";
 
 const config = require('./config');
-const AWS = require("aws-sdk");
+const AWS    = require("aws-sdk");
 
-//S3BucketPublicReadAccess
+const allUsersURI           = 'http://acs.amazonaws.com/groups/global/AllUsers'
+const fullControlPermission = "FULL_CONTROL"
+const readPermission        = "READ"
+const allUsersWriteAcp      = JSON.parse('{ "Grantee": { "Type": "Group", "URI": "http://acs.amazonaws.com/groups/global/AllUsers" }, "Permission": "WRITE_ACP" }')
+const allUsersWrite         = JSON.parse('{ "Grantee": { "Type": "Group", "URI": "http://acs.amazonaws.com/groups/global/AllUsers" }, "Permission": "WRITE" }')
+const allUsersReadAcp       = JSON.parse('{ "Grantee": { "Type": "Group", "URI": "http://acs.amazonaws.com/groups/global/AllUsers" }, "Permission": "READ_ACP" }')
+
+function remediateAllUsers(thisGrant, newAcl) {
+  if (thisGrant.Permission == fullControlPermission) { // in this case, apply all permission except READ
+    newAcl['Grants'].push(allUsersWrite);
+    newAcl['Grants'].push(allUsersReadAcp);
+    newAcl['Grants'].push(allUsersWriteAcp);
+  }
+  else if (thisGrant.Permission == readPermission) {  // do not pass through
+  }
+  else {
+    newAcl['Grants'].push(thisGrant);
+  }
+
+  return newAcl;
+}
+
+// look for and remove S3BucketPublicReadAccess
 module.exports.handler = (event, context, callback) => {
 
   console.log('S3 BucketPublicReadAccess - Received event:', JSON.stringify(event, null, 2));
@@ -14,36 +36,38 @@ module.exports.handler = (event, context, callback) => {
 
   var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-  // this code is included to provide a record for debugging
-  var params = {
+  var aclWas;
+  var aclNew = JSON.parse('{"Owner":"", "Grants":[]}'); // skeleton for new permission grants
+
+  var getAclParams = {
     Bucket: event.resource
   };
-  let getAclPromise = s3.getBucketAcl(params).promise();
+  let getAclPromise = s3.getBucketAcl(getAclParams).promise();
 
   getAclPromise
-    .then((data) => {
-       console.log(data);
-       console.log(data.grants);
-     })
-    .catch((err) => { console.log(err, err.stack) });
+    .then((aclWas) => {
+      aclNew.Owner = aclWas.Owner; // transfer the existing bucket owner
+      // now, act on any grants to all users - and just copy over any other grants
+      aclWas.Grants.forEach(function(grant, i){ if (grant.Grantee.URI == allUsersURI) {remediateAllUsers(grant, aclNew)} else {aclNew['Grants'].push(grant)}; })
+    })
+    .then(() => {
+      var putAclParams = {
+        Bucket: event.resource,
+        AccessControlPolicy: aclNew
+      };
+      let putAclPromise = s3.putBucketAcl(putAclParams).promise();
 
-  // actually close off access
-  var params = {
-    Bucket: event.resource,
-    ACL: "private",
-  };
-  let putAclPromise = s3.putBucketAcl(params).promise();
-
-  putAclPromise
-    .then((data) => {
-       console.log(data);
-       console.log(data.grants);
-     })
+      putAclPromise
+        .then((result) => {
+          console.log("result>" + JSON.stringify(result));
+        })
+    })
     .catch((err) => {
       console.log(err, err.stack);
-      callback(err, "Failed to auto-remediate S3-001");
-    });
+      callback(err, "failed to auto-remediate s3-001");
+    })
 
-  callback(null, "Success");
+ callback(null, "Success");
 
 };
+
