b31ba7b1a7a76803081e0d1c7d1ed789e1d83fad
bretwaldow <bret.waldow@mechanicalrock.io>
2018-02-14 10:24:15 +0800

    Refactor to expose local functions for unit testing, convert to S3-002

---
+++ functions/AutoRemediateS3-002.js
@@ -0,0 +1,77 @@
+"use strict";
+
+const config = require('./config');
+const AWS    = require("aws-sdk");
+
+const CCRuleCode     = 'S3-002'
+const CCRuleName     = 'BucketPublicReadAcpAccess'
+const allUsersURI    = 'http://acs.amazonaws.com/groups/global/AllUsers'
+const readPermission = "READ"
+
+function remediateAllUsers(thisGrant, newAcl) {
+  if (thisGrant.Permission != readPermission) {  // any besides READ are passed through
+     newAcl['Grants'].push(thisGrant);
+  }
+
+  return newAcl;
+}
+
+function transferOwner(oldAcl, newAcl) {
+ aclNew.Owner = aclWas.Owner; // transfer the existing bucket owner
+
+ return newAcl;
+}
+
+function transferAcl(oldAcl, newAcl) {
+  transferOwner(oldAcl, newAcl);
+
+  // now, act on any grants to all users - and just copy over any other grants
+  aclWas.Grants.forEach( function( grant, i ) { if ( grant.Grantee.URI == allUsersURI ) { remediateAllUsers( grant, aclNew ) } else { aclNew['Grants'].push( grant ) }; } )
+ 
+  return newAcl;
+}
+
+
+// look for and remove S3BucketPublicReadAccess
+module.exports.handler = (event, context, callback) => {
+
+  console.log('S3', CCRuleName, ' - Received event:', JSON.stringify(event, null, 2));
+
+  if (!event || !event.resource || event.ruleId !== CCRuleCode) {
+    return handleError('Invalid event');
+  }
+
+  var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+  var aclWas;
+  var aclNew = JSON.parse('{"Owner":"", "Grants":[]}'); // skeleton for new permission grants
+
+  var getAclParams = {
+    Bucket: event.resource
+  };
+  let getAclPromise = s3.getBucketAcl(getAclParams).promise();
+
+  getAclPromise
+    .then((aclWas) => {
+      transferAcl(aclWas, aclNew);
+    })
+    .then(() => {
+      const putAclParams = {
+        Bucket: event.resource,
+        AccessControlPolicy: aclNew
+      };
+      let putAclPromise = s3.putBucketAcl(putAclParams).promise();
+
+      putAclPromise
+        .then((result) => {
+          console.log('result>' + JSON.stringify(result));
+        })
+    })
+    .catch((err) => {
+      console.log(err, err.stack);
+      callback(err, 'failed to auto-remediate', CCRuleCode);
+    })
+
+ callback(null, 'Success');
+
+};
