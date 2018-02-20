c62ac8fdb4e714bf36e953cfedd5f37290429981
temyers <tim.myerscough@mechanicalrock.io>
2018-02-20 11:38:24 +0800

    WIP - add spec for S3_utils

---
+++ test/S3_utils.test.js
@@ -0,0 +1,27 @@
+// TODO - imports
+
+// TODO - remove DELETEME comments - for info only
+// DELETEME - describe the module under test
+describe("S3_utils", () => {
+
+    // DELETEME - describe each EXPORTED function in turn
+    describe("#removeAcpPermission()", () => {
+        // DELETEME Describe different contexts for the function
+        describe("When grant contains READ_ACP permission", () => {
+            // DELETEME describe the examples
+            // TODO - provide better textual descriptions for the spec.
+            it('should do something')
+        })
+
+        describe("When grant contains another permission", () => {
+            // describe the examples
+            it('should do something else')
+        })
+    })
+
+    describe("#transferOwner()", () => {
+        // TODO - fill me in
+    })
+
+    // TODO - test other exported functions
+})
