fb97728569c7a97c776df733790356c22f31d4bf
temyers <tim.myerscough@mechanicalrock.io>
2018-02-21 16:05:24 +0800

    Add failures

--- functions/AutoRemediateS3-002.js
+++ functions/AutoRemediateS3-002.js
@@ -11,13 +11,20 @@ const readAcpAllUsers = {
   Grantee: { Type: "Group", URI: "http://acs.amazonaws.com/groups/global/AllUsers" }, Permission: "READ_ACP"
 }
 
+function handleError(message, callback) {
+
+  message = message || "Failed to process request.";
+  return callback(new Error(message));
+
+}
+
 // look for and remove S3BucketPublicReadAccess
 const handler = (event, context, callback) => {
 
   console.log('S3', CCRuleName, ' - Received event:', JSON.stringify(event, null, 2));
 
   if (!event || !event.resource || event.ruleId !== CCRuleCode) {
-    return handleError('Invalid event');
+    return handleError('Invalid event', callback);
   }
 
   var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
