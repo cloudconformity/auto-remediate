14c54e5e0d21244f8f78251e9c0aac6ec2e154fe
bretwaldow <bret.waldow@mechanicalrock.io>
2018-03-07 12:14:20 +0800

    Format to project style

--- functions/AutoRemediateS3-005.js
+++ functions/AutoRemediateS3-005.js
@@ -1,43 +1,38 @@
-"use strict";
+'use strict'
 
-const config = require('./config');
-const  utils = require('../utils/S3_utils');
-const AWS = require("aws-sdk");
+const utils = require('../utils/S3_utils')
+const AWS = require('aws-sdk')
 
 const CCRuleCode = 'S3-005'
 const CCRuleName = 'BucketPublicReadAcpAccess'
 
 const readAcpAllUsers = {
-  Grantee: { Type: "Group", URI: "http://acs.amazonaws.com/groups/global/AllUsers" }, Permission: "FULL_CONTROL"
+  Grantee: { Type: 'Group', URI: 'http://acs.amazonaws.com/groups/global/AllUsers' }, Permission: 'FULL_CONTROL'
 }
 
-function handleError(message, callback) {
-
-  message = message || "Failed to process request.";
-  return callback(new Error(message));
-
+function handleError (message, callback) {
+  message = message || 'Failed to process request.'
+  return callback(new Error(message))
 }
 
 // look for and remove S3BucketPublicReadAccess
 const handler = (event, context, callback) => {
-
-  console.log('S3', CCRuleName, ' - Received event:', JSON.stringify(event, null, 2));
+  console.log('S3', CCRuleName, ' - Received event:', JSON.stringify(event, null, 2))
 
   if (!event || !event.resource || event.ruleId !== CCRuleCode) {
-    return handleError('Invalid event', callback);
+    return handleError('Invalid event', callback)
   }
 
-  var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+  var s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
   utils.filterAcl(s3, event.resource, readAcpAllUsers).then(() => {
-    callback(null, 'Success');
+    callback(null, 'Success')
   }).catch((err) => {
-    console.log(err, err.stack);
-    callback(err, 'failed to auto-remediate', CCRuleCode);
+    console.log(err, err.stack)
+    callback(err, 'failed to auto-remediate', CCRuleCode)
   })
-
-};
+}
 
 module.exports = {
-  handler: handler,
+  handler: handler
 }
