3675949e94f0114f192158976bbee07b3dbdb678
Cloud Dynamo <cloud2ynamo@gmail.com>
2018-09-25 16:27:55 +1000

    EC2-019 Unit Test added

---
+++ test/AutoRemediateEC2-019-test.js
@@ -0,0 +1,11 @@
+let event = {
+  'resource': 'ami-0afd9aa15c54e1210',
+  'region': 'ap-southeast-2'
+}
+
+let AutoRemediate = require('../functions/AutoRemediateEC2-019')
+
+AutoRemediate.handler(event, {}, function (err, data) {
+  console.log(err)
+  console.log('data', JSON.stringify(data, null, 2))
+})
