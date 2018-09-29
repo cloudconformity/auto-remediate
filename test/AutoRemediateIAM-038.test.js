81c9d371d72aeeb494f6611f6768e2144413cebe
Cloud Dynamo <rezafarshi@nbnco.com.au>
2018-09-29 17:48:38 +1000

    IAM-038 - Unit Test Added.

---
+++ test/AutoRemediateIAM-038.test.js
@@ -0,0 +1,10 @@
+let event = {
+  'resource': 'AKIAJRDQAM26ZHGO7VLA'
+}
+
+let AutoRemediate = require('../functions/AutoRemediateIAM-038')
+
+AutoRemediate.handler(event, {}, function (err, data) {
+  console.log(err)
+  console.log('data', JSON.stringify(data, null, 2))
+})
