50e116b2ba8423f486d23de91c1aa7fcbc280ac9
Bin Li <bin@cloudconformity.com>
2020-09-03 10:32:47 +1000

    fix: PD-4142 fixed sls deploy issue

--- stacks-map.js
+++ stacks-map.js
@@ -4,11 +4,5 @@ module.exports = {
   "AWS::Logs::LogGroup": { destination: "Log" },
   "AWS::Lambda::Version": { desination: "Function" },
   "AWS::S3::BucketPolicy": { desination: "Policy" },
-  "AWS::SQS::QueuePolicy": { desination: "Policy" },
-  "AWS::SNS::Subscription": { destination: "Infrastructure" },
-  "AWS::S3::Bucket": { destination: "Infrastructure" },
-  "AWS::KMS::Key": { destination: "Infrastructure" },
-  "AWS::KMS::Alias": { destination: "Infrastructure" },
-  "AWS::SNS::Topic": { destination: "Infrastructure" },
-  "AWS::SQS::Queue": { destination: "Infrastructure" }
+  "AWS::SQS::QueuePolicy": { desination: "Policy" }
 };
