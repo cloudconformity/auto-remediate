6c64a9196143809c262b32851229fff546bae12e
Bin Li <bin@cloudconformity.com>
2020-09-03 16:29:59 +1000

    fix: PD-4142 create a new stack splitter to resolve the undefinedNested stack

--- stacks-map.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "AWS::Lambda::Function": { destination: "Function" },
-  "AWS::IAM::Role": { destination: "Role" },
-  "AWS::Logs::LogGroup": { destination: "Log" },
-  "AWS::Lambda::Version": { desination: "Function" },
-  "AWS::S3::BucketPolicy": { desination: "Policy" },
-  "AWS::SQS::QueuePolicy": { desination: "Policy" }
-};
