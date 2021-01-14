bbd59fe620ccc82d9693251aba5e10574a16d41a
Chuck Mai <chuck_mai@trendmicro.com>
2021-01-14 10:36:55 +0800

    fix: PD-8650 refactor VPC-001

--- functions/AutoRemediateVPC-001.js
+++ functions/AutoRemediateVPC-001.js
@@ -116,9 +116,9 @@ module.exports.handler = (event, context, callback) => {
         return IAM.putRolePolicy(PutRolePolicyParams).promise().then(function () {
           console.log('Successfully put role policy')
         }).then(function () {
-          return IAM.getRole({ RoleName: VPCFlowLogRole }).promise().then(function (data) {
-            return data.Role.Arn
-          })
+          return IAM.getRole({ RoleName: VPCFlowLogRole }).promise()
+        }).then(function (data) {
+          return data.Role.Arn
         })
       })
     })
