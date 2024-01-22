fc159508e808436435b3b0543fd7d62fdcecaa5a
Nicholas Valiente <nicholas_valiente@trendmicro.com>
2024-01-22 15:42:40 +1100

    impr: CPO-302 update more functions to SDK V3

--- functions/AutoRemediateKMS-002.js
+++ functions/AutoRemediateKMS-002.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const AWS = require('aws-sdk')
+const { KMSClient, EnableKeyRotationCommand } = require('@aws-sdk/client-kms')
 
 /**
  * Lambda function to Enabled KMS Key Rotation
  */
 
-module.exports.handler = (event, context, callback) => {
+const handler = async (event, context, callback) => {
   console.log('Enable AWS KMS Key Rotation - Received event:', JSON.stringify(event, null, 2))
 
   if (!event || !event.resource) {
@@ -17,20 +17,21 @@ module.exports.handler = (event, context, callback) => {
     KeyId: event.resource
   }
 
-  const KMS = new AWS.KMS({ region: event.region })
+  const KMS = new KMSClient({ region: event.region })
 
-  KMS.enableKeyRotation(params, function (err, result) {
-    if (err) {
-      console.log('Error', err)
-      return handleError(err.message ? err.message : 'Enable Key Rotation failed')
-    }
-
+  try {
+    const result = await KMS.send(new EnableKeyRotationCommand(params))
     console.log('Result', result)
     return callback(null, 'Successfully processed event')
-  })
+  } catch (err) {
+    console.log('Error', err)
+    return handleError(err.message ? err.message : 'Enable Key Rotation failed')
+  }
 
   function handleError (message) {
     message = message || 'Failed to process request.'
     return callback(new Error(message))
   }
 }
+
+module.exports = handler
